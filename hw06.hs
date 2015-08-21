c22e3e51004b4a8d40cb73d43a6c7e28c0426f7c
Dmitry Cheryasov <dcheryasov@google.com>
2015-08-21 09:59:06 -0400

    Added matrix2 with multiplication.

--- hw06.hs
+++ hw06.hs
@@ -99,5 +99,29 @@ instance Num a => Num (Stream a) where
 instance Num a => Fractional (Stream a) where
   (/) (Stream a0 a_rest) (Stream b0 b_rest) = q where
     q = undefined -- Stream (a0 / b0) (1/b0 *- (a_rest - q * b_rest))
+    -- have to use the same haed + uncomputed-yet recursive call technique.
 
 
+-- 7: matrices
+
+{- Matrix2 a00 a01 a10 a11 in
+  /a00 a01\
+  \a10 a11/
+-}
+data Matrix2 = Matrix2 Integer Integer Integer Integer 
+
+instance Num Matrix2 where
+  fromInteger n = Matrix2 n 0 0 0
+  negate (Matrix2 x y z t) = Matrix2 (-x) (-y) (-z) (-t)
+  (+) (Matrix2 a00 a01 a10 a11) (Matrix2 b00 b01 b10 b11) =
+    Matrix2 (a00 + b00) (a01 + b01) (a10 + b10) (a11 + b11)
+  (*) (Matrix2 a00 a01 a10 a11) (Matrix2 b00 b01 b10 b11) =
+    Matrix2 c00 c01 c10 c11  where
+      c00 = (a00 * b00 + a01 * b10) 
+      c01 = (a00 * b01 + a01 * b11) 
+      c10 = (a10 * b00 + a11 * b10)
+      c11 = (a10 * b01 + a11 * b11)
+
+instance Show Matrix2 where
+  show (Matrix2 a00 a01 a10 a11) = "Matrix 2 (" ++ show a00 ++ ", " ++ show a01 ++ " | " ++ 
+                                                   show a10 ++ ", " ++ show a11 ++ ")"
