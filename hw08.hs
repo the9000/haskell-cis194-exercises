0e96801aa8c7a226790ea81c2ff8cb915f4b080b
Dmitry Cheryasov <dcheryasov@google.com>
2015-10-12 13:32:38 -0400

    08: maxFun initially works.

--- hw08.hs
+++ hw08.hs
@@ -2,7 +2,7 @@
 
 import Data.Monoid
 import Data.Tree
-import Data.List (foldl')
+import Data.List (foldl', maximumBy)
 import Control.Monad (join)
 
 import Employee
@@ -30,5 +30,24 @@ treeFold func init tree = foldl' func init $ flattened tree
     where
       flattened (Node {rootLabel=r, subForest=sf}) = r:(join $ map flattened sf)
 
--- REPL helpers
-mkNode r sf = Node {rootLabel=r, subForest=sf}
+-- boss -> [(best possible list with subtree boss), (bpl without)] -> (bpl with, bpl without)
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss bpls = (our_bpl_with, our_bpl_without) where
+    (bpls_with, bpls_without) = unzip bpls
+    our_bpl_with = glCons boss (mconcat bpls_without) 
+    our_bpl_without = mconcat bpls_with
+
+-- 4
+-- A bit more compact than 
+-- tree level -> (bpl with node's boss, bpl without node's boss)
+onLevel :: Tree Employee -> (GuestList, GuestList)
+onLevel (Node boss []) = (GL [boss] (empFun boss), GL [] 0)
+onLevel (Node boss subs) = (bpl_with_boss, bpl_without_boss) where
+    (bpls_with, bpls_without) = unzip $ map onLevel subs                 
+    bpl_with_boss = glCons boss (mconcat bpls_without)
+    bpl_without_boss = mconcat bpls_with                              
+
+
+maxFun :: Tree Employee -> GuestList
+maxFun = uncurry max $ onLevel
+
