eca217cc4f1f6b6351229bbcdec9b272dc7dfea6
Dmitry Cheryasov <dcheryasov@google.com>
2015-10-13 18:53:02 -0400

    Raw I/O works; lacks formatting.

--- hw08.hs
+++ hw08.hs
@@ -5,6 +5,8 @@ import Data.Tree
 import Data.List (foldl', maximumBy)
 import Control.Monad (join)
 
+import System.IO
+
 import Employee
 
 -- 1
@@ -25,11 +27,14 @@ moreFun g1@(GL _ f1) g2@(GL _ f2) = if f1 >= f2 then g1 else g2
 
 -- 2
 
+-- Since we have no (b -> b -> b), we can't attach results from recursive calls.
 treeFold :: (b -> a -> b) -> b -> Tree a -> b 
 treeFold func init tree = foldl' func init $ flattened tree 
     where
       flattened (Node {rootLabel=r, subForest=sf}) = r:(join $ map flattened sf)
 
+-- 3
+-- Not really useful for folding, see signature.
 -- boss -> [(best possible list with subtree boss), (bpl without)] -> (bpl with, bpl without)
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
 nextLevel boss bpls = (our_bpl_with, our_bpl_without) where
@@ -38,7 +43,7 @@ nextLevel boss bpls = (our_bpl_with, our_bpl_without) where
     our_bpl_without = mconcat bpls_with
 
 -- 4
--- A bit more compact than 
+-- A bit more compact than nextLevel.
 -- tree level -> (bpl with node's boss, bpl without node's boss)
 onLevel :: Tree Employee -> (GuestList, GuestList)
 onLevel (Node boss []) = (GL [boss] (empFun boss), GL [] 0)
@@ -51,3 +56,11 @@ onLevel (Node boss subs) = (bpl_with_boss, bpl_without_boss) where
 maxFun :: Tree Employee -> GuestList
 maxFun = uncurry max . onLevel
 
+-- 5: IO
+
+main :: IO ()
+main = do
+  handle <- openFile "company.txt" ReadMode
+  contents <- hGetContents handle
+  let tree = read contents :: Tree Employee
+  putStrLn ("Top is " ++ (show $ maxFun tree))
